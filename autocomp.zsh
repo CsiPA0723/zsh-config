7feac00d1a3beaab32cca66300d609c8aa1fb418
Péter Csizmadia <csizipeti0723@gmail.com>
2025-04-08 02:13:44 +0200

    Add conditional load for server

--- autocomp.zsh
+++ autocomp.zsh
@@ -1,10 +1,13 @@
 # Zoxide
 eval "$(zoxide init zsh)"
 
-# Python pipx
-eval "$(register-python-argcomplete pipx)"
+# Load only on Laptop
+if [[ $(uname -ar) = *"CsiPA-Arch"* ]]; then
+  # Python pipx
+  eval "$(register-python-argcomplete pipx)"
 
-# Load Angular CLI autocompletion.
-source <(ng completion script)
+  # Load Angular CLI autocompletion.
+  eval "$(ng completion script)"
+fi
 
 # vim:fileencoding=utf-8:foldmethod=marker
--- zshenv.zsh
+++ zshenv.zsh
@@ -1,10 +1,14 @@
-export SSH_AUTH_SOCK="$XDG_RUNTIME_DIR/ssh-agent.socket"
-export DOCKER_HOST="unix://$XDG_RUNTIME_DIR/docker.sock"
+# Load only on Laptop
+if [[ $(uname -ar) = *"CsiPA-Arch"* ]]; then
+  export SSH_AUTH_SOCK="$XDG_RUNTIME_DIR/ssh-agent.socket"
+  export DOCKER_HOST="unix://$XDG_RUNTIME_DIR/docker.sock"
+  export PSTEST_PROFILE_LOAD='6b8359bb-63ba-45b3-bcf0-b7beb9cbffc6'
+  export npm_config_prefix="$HOME/.local"
+fi
+
 export BAT_THEME="Catppuccin Macchiato"
 export MANPAGER="nvim +Man! "
-export PSTEST_PROFILE_LOAD='6b8359bb-63ba-45b3-bcf0-b7beb9cbffc6'
 export AUR_PAGER="yazi"
-export npm_config_prefix="$HOME/.local"
 
 typeset -U path PATH
 path=(~/.local/bin $path)
