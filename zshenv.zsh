9b05dc4a7df3e6c1a2ed5d3bc20347e6f2770a17
Péter Csizmadia <csizipeti0723@gmail.com>
2025-10-21 14:55:27 +0200

    Export GH_TOKEN

--- zshenv.zsh
+++ zshenv.zsh
@@ -5,6 +5,7 @@ if [[ $(uname -ar) = *"CsiPA-Arch"* ]]; then
   export SSH_AUTH_SOCK="$XDG_RUNTIME_DIR/ssh-agent.socket"
   export DOCKER_HOST="unix://$XDG_RUNTIME_DIR/docker.sock"
   export PSTEST_PROFILE_LOAD='6b8359bb-63ba-45b3-bcf0-b7beb9cbffc6'
+  export GH_TOKEN=$(gh auth token)
 
   export MANPATH=/usr/local/texlive/2013/texmf-dist/doc/man:$MANPATH
   export INFOPATH=/usr/local/texlive/2013/texmf-dist/doc/info:$INFOPATH
